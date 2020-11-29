a7f5ca2b2c174d252d1e0fad3836f93dcb643b5c
adibrisan <adi.brisan@yahoo.com>
2020-11-29 15:52:13 +0200

    Code Challenge 2

--- app/src/main/java/com/android/androidfundamentalsgroup1/MainActivity.java
+++ app/src/main/java/com/android/androidfundamentalsgroup1/MainActivity.java
@@ -13,12 +13,6 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView oneTextView;
-    private EditText editTextUserFullName;
-    private CheckBox checkBoxTermsAndConditions;
-    private SeekBar seekBarCountChallenges;
-    private TextView textViewPurpleContent;
-    private Button buttonGetContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,64 +20,10 @@ public class MainActivity extends AppCompatActivity {
         // seteaza layout-ul asociat activatii MainActivity
         // setContentView(R.layout.activity_main);
         // asociem views_sample_1 ca layout al activitatii MainActivity
-        setContentView(R.layout.views_sample_1);
-
-        oneTextView = findViewById(R.id.firstTextView);
-        oneTextView.setText(R.string.new_text);
-
-        editTextUserFullName = findViewById(R.id.editTextFullName);
-        editTextUserFullName.setText(R.string.default_full_name);
-
-        checkBoxTermsAndConditions = findViewById(R.id.checkboxTermsAndConditions);
-        if (checkBoxTermsAndConditions.isChecked()) {
-            checkBoxTermsAndConditions.setChecked(false);
-            oneTextView.setText(R.string.checkbox_checked);
-        } else {
-            checkBoxTermsAndConditions.setChecked(true);
-            oneTextView.setText(R.string.checkbox_unchecked);
-        }
-
-        checkBoxTermsAndConditions.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                if (compoundButton.isChecked()) {
-                    editTextUserFullName.setText(R.string.checkbox_state_checked);
-                } else {
-                    editTextUserFullName.setText(R.string.checkbox_state_unchecked);
-                }
-            }
-        });
-
-        seekBarCountChallenges = findViewById(R.id.seekBarChallenges);
-        seekBarCountChallenges.setProgress(5);
-
-        textViewPurpleContent = findViewById(R.id.textViewPurpleContent);
-
-        buttonGetContent = findViewById(R.id.buttonGetContent);
-        // block comment/un-comment CTRL + Shift + /
-        /*buttonGetContent.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // preluam contentul din EditText si il convertim in String
-                String contentFromEditText = editTextUserFullName.getText().toString();
-                if (contentFromEditText != null && contentFromEditText.length() > 0) {
-                    textViewPurpleContent.setText(contentFromEditText);
-                } else {
-                    editTextUserFullName.setError(getString(R.string.error_missing_text));
-                }
-            }
-        });*/
-
-    }
+        setContentView(R.layout.adrian);
 
 
-    public void buttonGetContentOnClick(View view) {
-        // preluam contentul din EditText si il convertim in String
-        String contentFromEditText = editTextUserFullName.getText().toString();
-        if (contentFromEditText != null && contentFromEditText.length() > 0) {
-            textViewPurpleContent.setText(contentFromEditText);
-        } else {
-            editTextUserFullName.setError(getString(R.string.error_missing_text));
-        }
     }
 }
+
+
