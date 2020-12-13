11ac175ac35a24670cf5d2d74575f6306aea3c91
Gelu Ungur <ImGelu@gmail.com>
2020-12-13 18:02:22 +0200

    Updated template

--- app/src/main/java/com/android/androidfundamentalsgroup1/MainActivity.java
+++ app/src/main/java/com/android/androidfundamentalsgroup1/MainActivity.java
@@ -3,36 +3,12 @@ package com.android.androidfundamentalsgroup1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Patterns;
-import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.EditText;
-import android.widget.SeekBar;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-    private EditText emailInput;
-    private EditText phoneInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_view);
-    }
-
-    public void buttonSubmitOnClick(View view){
-        emailInput = findViewById(R.id.emailInput);
-        phoneInput = findViewById(R.id.phoneInput);
-
-        String emailInputContent = emailInput.getText().toString();
-        String phoneInputContent = phoneInput.getText().toString();
-
-        if(!Patterns.EMAIL_ADDRESS.matcher(emailInputContent).matches()) {
-            emailInput.setError(getString(R.string.invalid_email));
-        }
+        setContentView(R.layout.activity_main);
     }
 }
