ba07538d4b609cf059f694adc84e3d1ee392f2bf
Gelu Ungur <ImGelu@gmail.com>
2020-12-22 19:40:17 +0200

    Week 5, Challenge 3

--- app/src/main/java/com/android/androidfundamentalsgroup1/MainActivity.java
+++ app/src/main/java/com/android/androidfundamentalsgroup1/MainActivity.java
@@ -4,42 +4,49 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button buttonTextOne;
-    private Button buttonTextTwo;
-    private Button buttonTextThree;
+    private Button buttonSend;
+    private EditText messageInput;
+    private TextView titleTextView, messageTextView;
 
-    public static final String TEXT_KEY = "text";
+    public static final String TEXT_KEY = "message";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        buttonTextOne = findViewById(R.id.buttonTextOne);
-        buttonTextTwo = findViewById(R.id.buttonTextTwo);
-        buttonTextThree = findViewById(R.id.buttonTextThree);
+        buttonSend = findViewById(R.id.buttonSendPrimary);
+        messageInput = findViewById(R.id.editTextMessagePrimary);
+        titleTextView = findViewById(R.id.textViewTitlePrimary);
+        messageTextView = findViewById(R.id.textViewMessagePrimary);
+
+        Bundle bundle = getIntent().getExtras();
+
+        if(bundle != null) {
+            titleTextView.setVisibility(View.VISIBLE);
+            messageTextView.setVisibility(View.VISIBLE);
+
+            String text = bundle.getString(SecondActivity.TEXT_KEY);
+            messageTextView.setText(text);
+        } else {
+            titleTextView.setVisibility(View.GONE);
+            messageTextView.setVisibility(View.GONE);
+        }
     }
 
-    public void buttonOneOpenSecondActivity(View view) {
+    public void buttonSendMessage(View view) {
         Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-        intent.putExtra(TEXT_KEY, getString(R.string.dummy_text_1));
-        startActivity(intent);
-    }
+        String message = messageInput.getText().toString();
 
-    public void buttonTwoOpenSecondActivity(View view) {
-        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-        intent.putExtra(TEXT_KEY, getString(R.string.dummy_text_2));
-        startActivity(intent);
-    }
-
-    public void buttonThreeOpenSecondActivity(View view) {
-        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-        intent.putExtra(TEXT_KEY, getString(R.string.dummy_text_3));
+        intent.putExtra(TEXT_KEY, message);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/android/androidfundamentalsgroup1/SecondActivity.java
+++ app/src/main/java/com/android/androidfundamentalsgroup1/SecondActivity.java
@@ -1,26 +1,51 @@
 package com.android.androidfundamentalsgroup1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
 
-    TextView secondActivityTextView;
+    private Button buttonSend;
+    private EditText messageInput;
+    private TextView titleTextView, messageTextView;
+
+    public static final String TEXT_KEY = "message";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        secondActivityTextView = findViewById(R.id.secondActivityTextView);
+        buttonSend = findViewById(R.id.buttonSendSecondary);
+        messageInput = findViewById(R.id.editTextMessageSecondary);
+        titleTextView = findViewById(R.id.textViewTitleSecondary);
+        messageTextView = findViewById(R.id.textViewMessageSecondary);
 
         Bundle bundle = getIntent().getExtras();
 
-        if(bundle != null){
-            String text = bundle.getString(MainActivity.TEXT_KEY);
-            secondActivityTextView.setText(text);
+        if(bundle != null) {
+            titleTextView.setVisibility(View.VISIBLE);
+            messageTextView.setVisibility(View.VISIBLE);
+
+            String text = bundle.getString(SecondActivity.TEXT_KEY);
+            messageTextView.setText(text);
+        } else {
+            titleTextView.setVisibility(View.GONE);
+            messageTextView.setVisibility(View.GONE);
         }
+    }
+
+    public void buttonSendMessage(View view) {
+        Intent intent = new Intent(SecondActivity.this, MainActivity.class);
+        String message = messageInput.getText().toString();
+
+        intent.putExtra(TEXT_KEY, message);
+        startActivity(intent);
     }
 }
