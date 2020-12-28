41a8fa3f5d46ab12fbcfaf2ffaa98119d32ffe69
livalex <livadarualex@gmail.com>
2020-12-29 01:21:46 +0200

    Alexandru Livadaru | Code Challange 1, 2, 3, 4 | 5 Activities and Intents

---
+++ exercise1/app/src/main/java/com/example/codechallange5/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.codechallange5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.i(TAG, "onCreate");
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
+    }
+
+    public void openActivity2() {
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "onRestart");
+    }
+}
---
+++ exercise2/app/src/main/java/com/example/codechallange2/MainActivity3.java
@@ -0,0 +1,39 @@
+package com.example.codechallange2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ScrollView;
+
+public class MainActivity3 extends AppCompatActivity {
+    ScrollView sv;
+    private int offset = 0;
+
+    private final void focusOnView(){
+        sv.post(new Runnable() {
+            @Override
+            public void run() {
+                sv.scrollTo(0, offset);
+            }
+        });
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+
+        if (getIntent().hasExtra("selected_paragraph")) {
+            offset = getIntent().getIntExtra("selected_paragraph", 0);
+
+            if (offset == 2) {
+                offset = 2000;
+            } else if (offset == 3) {
+                offset = 4000;
+            }
+        }
+
+        sv = (ScrollView)findViewById(R.id.scrollView_text3);
+        focusOnView();
+    }
+}
